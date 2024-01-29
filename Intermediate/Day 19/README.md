364cfa6fcc695faee07dabf967e4df70a03f9ec3
Gabriel Germano <gabriel-germano333@hotmail.com>
2024-01-29 01:35:08 -0300

    Day 19

--- Intermediate/Day 19/README.md
+++ Intermediate/Day 19/README.md
@@ -1,5 +1,5 @@
 # Day 19 - Instances, State and Higher Order Functions
 
-## The Hirst Painting Project
-![PaintingProjectGIF](PaintingProjectGIF.gif)
+## Turtle Race Project
+![TurtleRaceGif](TurtleRaceGif.gif)
 
--- Intermediate/Day 19/TurtleRace.py
+++ Intermediate/Day 19/TurtleRace.py
@@ -1,8 +1,44 @@
 from turtle import Turtle, Screen
+import random
 
-toni = Turtle()
 screen = Screen()
-screen.setup(500, 400)
+screen.setup(width=1800, height=600)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+
+is_race_on = False
+all_turtles = []
+for turtle_num in range(0, 6):
+    toni = Turtle(shape="turtle")
+    all_turtles.append(toni)
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+bottom_turtle_x = -880
+bottom_turtle_y = -150
+for turtle_position in range(0, len(all_turtles)):
+    all_turtles[turtle_position].fillcolor(colors[turtle_position])
+    all_turtles[turtle_position].speed(4)
+    all_turtles[turtle_position].shapesize(2, 2)
+    all_turtles[turtle_position].up()
+    all_turtles[turtle_position].goto(x= bottom_turtle_x, y=bottom_turtle_y)
+    bottom_turtle_y += 60
+
+if user_bet:
+    is_race_on = True
 
 
+winner = ''
+while is_race_on:
+    for turtle_position in range(0, len(all_turtles)):
+        all_turtles[turtle_position].forward(random.randint(0, 20))
+        if all_turtles[turtle_position].pos()[0] >= 860:
+            winner = all_turtles[turtle_position]
+            is_race_on = False
+
+if user_bet == winner.fillcolor():
+    print("You won!")
+else:
+    print("You lost!")
+
+print(f"The winner is {winner.fillcolor()}!" )
 screen.exitonclick()
