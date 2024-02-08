b424ce3f2a2de9f064817faa091cd0d0bec7c9ec
Gabriel Germano <gabriel-germano333@hotmail.com>
2024-02-08 06:15:18 -0300

    Day 25

--- Intermediate/Day 25/main.py
+++ Intermediate/Day 25/main.py
@@ -1,16 +1,38 @@
-# import csv
-
-# with open("weather_data.csv") as data_file:
-#     csv_data = csv.reader(data_file)
-#     temperature_list = []
-#     for row in csv_data:
-#         temperature_list.append(int(row[1]))
-# print(temperature_list)
-
+import turtle
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-temp_list = data["temp"]
+def write_state_name(state_name, x, y):
+    new_state_name = turtle.Turtle()
+    new_state_name.hideturtle()
+    new_state_name.penup()
+    new_state_name.goto((x, y))
+    new_state_name.write(state_name, align="center", font=('Arial', 8, 'normal'))
+    
+screen = turtle.Screen()
+screen.setup(width=730, height=495)
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.Turtle().shape(image)
 
-print(temp_list.mean())
-print(temp_list.max())
+us_data = pandas.read_csv("50_states.csv")
+us_states_list = us_data.state.to_list()
+
+correct_guesses = []
+while len(correct_guesses) < 50:
+    screen.title(f"U.S States Game  - {len(correct_guesses)}/50")
+    user_guess = turtle.textinput(title="U.S States Game", prompt="Make your guess!").title()
+    if user_guess == "Exit":
+        break
+    else:
+        if user_guess not in correct_guesses:
+            if user_guess in us_states_list:
+                correct_guesses.append(user_guess)
+                state_info = us_data[us_data.state == user_guess]
+                write_state_name(user_guess, int(state_info.x), int(state_info.y))
+
+states_to_learn = []
+for state in us_states_list:
+    if state not in correct_guesses:
+        states_to_learn.append(state)
+pandas.DataFrame(states_to_learn).to_csv("states_to_learn.csv")
+screen.mainloop()
